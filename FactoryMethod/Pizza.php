3ec6acd125ef8f13329d88d75d037c524af010c7
marchtea <marchtea213@gmail.com>
2013-12-30 21:12:48 +0800

    finish factory method

--- FactoryMethod/Pizza.php
+++ FactoryMethod/Pizza.php
@@ -1,9 +1,21 @@
 <?php
-namespace SimpleFactory;
+namespace FactoryMethod;
 
 class Pizza{
+	protected $name;
+	protected $dough;
+	protected $sauce;
+	protected $topping;
+
 	public function prepare(){
-		print "Pizza prepared.\n";
+		print "preparing $this->name\n";
+		print "Tossing dough: $this->dough\n";
+		print "Adding sauce: $this->sauce\n";
+		print "Adding toppings:";
+		for($i = 0;$i != count($this->topping); $i++){
+			print "\t".$this->topping[$i];
+		}
+		print "\nPizza prepared.\n";
 	}
 	public function bake(){
 		print "Pizza baked.\n";
@@ -17,6 +29,13 @@ class Pizza{
 }
 
 class CheesePizza extends Pizza{
+	function __construct(){
+		$this->name = "Cheese Pizza";
+		$this->dough = 'Thin Crust Dough';
+		$this->sauce = 'Marinara Sauce';
+
+		$this->topping = array('Grated Reggiano Cheese');
+	}
 
 }
 
--- FactoryMethod/PizzaStore.php
+++ FactoryMethod/PizzaStore.php
@@ -1,26 +1,46 @@
 <?php
 
-class PizzaStore {
+namespace FactoryMethod;
+
+include 'Pizza.php';
+
+abstract class PizzaStore {
 	function test(){
 		print "test";
 	}
 
 	public function orderPizza($type){
-		$pizza = null;
-		$pizza = createPizza($type);
+		$pizza = $this->createPizza($type);
 
-		$pizza.prepare();
-		$pizza.bake();
-		$pizza.cut();
-		$pizza.box();
+		$pizza->prepare();
+		$pizza->bake();
+		$pizza->cut();
+		$pizza->box();
+
+		return $pizza;
 
 	}
-
-
+	abstract function createPizza($type);
 }
 
-$store = new PizzaStore();
-$store->test();
+class nyPizzaStore extends PizzaStore{
+	function createPizza($type){
+		$pizza = null;
+		if (strcmp($type, 'cheese') == 0){
+			$pizza = new CheesePizza();
+		}else if (strcmp($type, 'pepperoni') == 0){
+			$pizza = new PepperoniPizza();
+		}else if (strcmp($type, 'clam') == 0){
+			$pizza = new ClamPizza();
+		}else if (strcmp($type, 'veggie') == 0){
+			$pizza = new VeggiePizza();
+		}
+		return $pizza;
+	}
+}
+
+$store = new nyPizzaStore();
+$store->orderPizza("cheese");
 
 
 
